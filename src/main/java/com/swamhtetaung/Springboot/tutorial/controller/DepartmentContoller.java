08ed04bb2a73985c6c44ec79a3fa77244cb985b5
Swam Htet Aung <swamhtetaungg@gmail.com>
2023-08-16 11:35:14 +0630

    Add logger

--- src/main/java/com/swamhtetaung/Springboot/tutorial/controller/DepartmentContoller.java
+++ src/main/java/com/swamhtetaung/Springboot/tutorial/controller/DepartmentContoller.java
@@ -3,6 +3,8 @@ package com.swamhtetaung.Springboot.tutorial.controller;
 import com.swamhtetaung.Springboot.tutorial.entity.Department;
 import com.swamhtetaung.Springboot.tutorial.service.DepartmentService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +15,18 @@ public class DepartmentContoller {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentContoller.class);
+
+
     @PostMapping("/department")
     public Department storeDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside storeDepartment");
         return departmentService.storeDepartment(department);
     }
 
     @GetMapping("/department")
     public List<Department> getDepartments(){
+        LOGGER.info("Inside getDepartments");
         return departmentService.getDepartments();
     }
 
